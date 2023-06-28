35db8a39871ef91c56b83adcdf18f51d7677369c
bokoko33 <work.bokoko33@gmail.com>
2023-06-28 23:49:28 +0900

    setup meta

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,27 @@
 import { PageAbout } from '~/components/PageAbout';
 
-export default function Home() {
+export async function generateMetadata() {
+  const title = 'About | Next.js Template';
+  const description = 'This is Next.js Template';
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: 'localhost:3000',
+      siteName: title,
+      locale: 'ja_JP',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+    },
+  };
+}
+
+export default function About() {
   return <PageAbout />;
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
 import { PageIndex } from '~/components/PageIndex';
 
+export async function generateMetadata() {
+  const title = 'Next.js Template';
+  const description = 'This is Next.js Template';
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: 'localhost:3000',
+      siteName: title,
+      locale: 'ja_JP',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+    },
+  };
+}
+
 export default function Home() {
   return <PageIndex />;
 }
